6ee29cd363873eae4cc7a5f390227585e08b536d
443studio <yoshimi1039@gmail.com>
2017-06-18 19:44:06 +0900

    main.py
    Minor change

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import time
 import MatrixLED4Col
+import RPi.GPIO as GPIO
 
 #Constants
-LEDSIZE = 16 
+RATCH = 3
+AnSIK = 4
+GrnSIK = 5
+RedSIK = 6
+AnSI = 7
+GrnSI = 8
+RedSI = 9
+
+LEDSIZE = 16
+
+
 DummyPtnList = [[6],[6],[6],[6],[6],[6],[6],[6],[6],[6],[6],[6],[6],[6],[6],[6]]
 #0=K, 2=Grn, 3=Red, 6=Org
 
-MatrixLED = MatrixLED4Col(LEDSIZE, "RED", "GREEN")
+MatrixLED = MatrixLED4Col(LEDSIZE, RATCH, SIK = [AnSIK,GrnSIK,RedSIK], SI = [AnSI, GrnSI, RedSI])
 
 MatrixLED.push(DummyPtnList)
 MatrixLED.flashLED()
